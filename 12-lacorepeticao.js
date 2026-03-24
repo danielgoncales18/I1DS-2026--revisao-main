3507ef32bd17f6ff9cfdf6e5ab664d0600281316
danielgoncales18 <danielgoncalesferreira16@gmail.com>
2026-03-24 10:15:49 -0300

    first commit

---
+++ 12-lacorepeticao.js
@@ -0,0 +1,14 @@
+//           laco de repetiçao - for (para)
+// o professor te colocou de castigo e pediu para voce escrever mil vezes a frase
+// "eu vou prestar atencao as aulas e anotar tudo"
+
+for (let i = 0; i < 1000; i++) { // i++ == i = 
+    
+    
+}
+
+
+
+
+
+
